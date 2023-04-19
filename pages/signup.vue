d53e26b6479c59f98be0de443bd179fad89749e6
Chefback <chefbacksw@gmail.com>
2023-04-19 12:05:11 +0800

    update signup logic

--- pages/signup.vue
+++ pages/signup.vue
@@ -3,7 +3,7 @@
     <v-container class="text-center">
         <v-row :align="'center'" :justify="'center'" class="mt-12">
             <v-col cols="12" md="6" lg="3">
-                <authenticate-form button-title="Register" form.sync="form"></authenticate-form>
+                <authenticate-form button-title="Register" :form.sync="form"></authenticate-form>
             </v-col>
         </v-row>
         <snack-bar :snackbar-message.sync="snackbarMessage"></snack-bar>
@@ -15,24 +15,26 @@ import SnackBar from '@/components/snackBar'
 
 export default {
     layout: 'login',
-    components: { AuthenticationForm, SnackBar },
-    data: () => ({
-        form: {
-            valid: false,
-            email: '',
-            password: '',
-            finish: false
-        },
-        snackbarMessage: ''
-    }),
+    components: { AuthenticateForm, SnackBar },
+    data() {
+        return {
+            form: {
+                valid: false,
+                name: '',
+                password: '',
+                finish: false
+            },
+            snackbarMessage: 'ladh'
+        }
+    },
     computed: {
         finish() {
             return this.form.finish
         }
     },
     watch: {
-        finish(newVal) {
-            if (newVal) {
+        finish(newval) {
+            if (newval) {
                 this.register()
                 this.form.finish = false
             }
@@ -41,25 +43,25 @@ export default {
     methods: {
         async register() {
             try {
-                await this.$axios.post('/api/auth/register', {
-                    email: this.form.email,
+                const admin = {
+                    name: this.form.name,
                     password: this.form.password
+                };
+                const response = await this.$axios.post('/api/auth/register', admin)
+
+                await this.$auth.loginWith('local', {
+                    data: admin
                 })
 
-                const user = await this.$auth.loginWith('local', {
-                    data: {
-                        email: this.form.email,
-                        password: this.form.password
-                    }
-                })
-
-                if (user) {
-                    await this.$router.push('/admin')
-                }
+                this.snackbar = true
+                this.snackbarMessage = response.data.message
+                await this.$router.push('/')
 
             } catch (error) {
+                this.snackbar = true
                 this.snackbarMessage = error.response.data.message
             }
+
         }
     }
 }
