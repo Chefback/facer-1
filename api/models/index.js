9e1b17eab7dd6d745cfa7136ecd3c664cc1c8eea
Chefback <chefbacksw@gmail.com>
2023-04-19 12:00:11 +0800

    add timestamp,unique property,imgSchema

--- api/models/index.js
+++ api/models/index.js
@@ -1,20 +1,27 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 //创建Schema
+const imgSchema = new Schema({
+    filename: { type: String, required: true },
+    contentType: { type: String, required: true },
+    data: { type: String, required: true },
+}, { timestamps: true })
+
 const userSchema = new Schema({
     name: { type: String, index: { unique: true, dropDups: true } },
     sex: String,
     phone: String,
-    date: { type: Date, default: Date.now },
     photos: [mongoose.Types.ObjectId]
-})
+}, { timestamps: true })
 
 const adminSchema = new Schema({
-    username: String,
-    password: String
-})
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true })
+
 
 const User = mongoose.model('User', userSchema);
 const Admin = mongoose.model('Admin', adminSchema);
+const Image = mongoose.model('Image', imgSchema);
 
-module.exports = { User, Admin };
+module.exports = { User, Admin, Image };
