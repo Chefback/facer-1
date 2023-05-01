4ef95796e17a0021a786c5071d6adc06df8a5738
Chefback <chefbacksw@gmail.com>
2023-05-01 12:49:46 +0800

    fix(userStore): fix create,update,download

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,7 +1,6 @@
 const { User } = require('../models')
 const mongoose = require('mongoose')
 const upload = require("./upload")
-
 const config = require('../models/config');
 
 const url = config.url + config.database;
@@ -19,9 +18,10 @@ connect.once('open', () => {
 
 // Create and Save a new User
 exports.create = async (req, res) => {
+    console.log(req.body)
     res.header("Content-Type", "application/json")
     let newUser = new User(
-        req.body
+        req.body.user
     );
 
     newUser.save()
@@ -34,19 +34,13 @@ exports.create = async (req, res) => {
         .catch(err => res.status(500).json(err));
 }
 
-/**
- * Delete a user and all its photos 
- *
- * @param {*} req pass in username
- * @param {*} res send delete result
- */
 exports.delete = async (req, res) => {
     const user = await User.findOneAndDelete({ name: req.body.user })
     const files = user.photos
     console.log(files.length)
     if (files.length != 0) {
         files.forEach((file) => {
-            gfs.delete(file)
+            gfs.remove(file)
                 .then(() => {
                     res.status(200).json({
                         success: true,
@@ -66,9 +60,11 @@ exports.delete = async (req, res) => {
 };
 
 exports.update = async (req, res) => {
+    console.log(req.body)
     await User.findByIdAndUpdate(
-        req.body._id, req.body.newdata, { new: true })
+        req.body.user._id, req.body.user, { new: true })
         .then((user) => {
+            console.log(user)
             res.status(200).json({
                 success: true,
                 user,
@@ -82,21 +78,10 @@ exports.upload = async (req, res) => {
         await upload(req, res).then(async () => {
             req.files.map(async (file) => {
                 const fileid = new mongoose.Types.ObjectId(file.id)
-                // const base64Data = file.buffer.toString('base64')
-                // let newfile = new Image({
-                //     filename: file.originalname,
-                //     contentType: file.mimetype,
-                //     data: base64Data
-                // })
 
                 await User.updateOne(
                     { name: req.body.user }, { $push: { photos: fileid } })
             })
-            // newfile.save().then((photo) => {
-            //     res.status(200).json(
-            //         photo
-            //     )
-            // })
         });
         console.log(req.files, req.body.user);
 
@@ -129,45 +114,27 @@ exports.getAll = async (req, res) => {
         });
 }
 
-// exports.getPhoto = async (req, res) => {
-//     await User.findOne(req.body.user).then((user) => {
-//         user.photos.forEach((i) => {
-//             gfs.find({ i })
-//             cursor.forEach(file => gfs.delete(file._id))
-//         })
-//     })
-//         .catch(err => {
-//             res.status(500).send({
-//                 message:
-//                     err.message || "Some error occurred while deleting photos."
-//             });
-//         });
-// }
 exports.download = async (req, res) => {
-    try {
 
-        const fileid = new mongoose.Types.ObjectId(req.params.id)
-        console.log(req.params)
-        res.contentType('image/png');
-        let downloadStream = await gfs.openDownloadStream(fileid)
-        // res.pipe(downloadStream)
-        downloadStream.on("data", function (data) {
-            console.log(data)
-            return res.status(200).write(data);
-        });
+    const file_id = new mongoose.Types.ObjectId(req.params.file)
 
-        downloadStream.on("error", function (err) {
-            return res.status(404).send({ message: "Cannot download the Image!" });
-        });
-
-        downloadStream.on("end", () => {
-            return res.end();
-        });
-    } catch (error) {
-        return res.status(500).send({
-            message: error.message,
+    const files = await gfs.find({ _id: file_id }).toArray()
+    if (!files || files.length === 0) {
+        return res.status(200).json({
+            success: false,
+            message: 'No files available'
         });
     }
+    if (files[0].contentType === 'image/jpeg' || files[0].contentType === 'image/png' || files[0].contentType === 'image/svg+xml') {
+        // render image to browser
+        res.type(files[0].contentType)
+        gfs.openDownloadStream(file_id).pipe(res);
+    } else {
+        res.status(404).json({
+            err: 'Not an image',
+        });
+    }
+    console.log(files, 'single_file')
 };
 
 
