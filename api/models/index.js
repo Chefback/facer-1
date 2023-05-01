398c4fd610872381ae4e5e787549c0f168ff214f
Chefback <chefbacksw@gmail.com>
2023-05-01 12:50:39 +0800

    refactor(database): remove imagemodel

--- api/models/index.js
+++ api/models/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 //创建Schema
-const imgSchema = new Schema({
-    filename: { type: String, required: true },
-    contentType: { type: String, required: true },
-    data: { type: String, required: true },
-}, { timestamps: true })
+// const imgSchema = new Schema({
+//     filename: { type: String, required: true },
+//     contentType: { type: String, required: true },
+//     data: { type: String, required: true },
+// }, { timestamps: true })
 
 const userSchema = new Schema({
     name: { type: String, index: { unique: true, dropDups: true } },
@@ -22,6 +22,6 @@ const adminSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 const Admin = mongoose.model('Admin', adminSchema);
-const Image = mongoose.model('Image', imgSchema);
+// const Image = mongoose.model('Image', imgSchema);
 
 module.exports = { User, Admin };
